d77e6d9cb34ba6b6ab89cf02c1377944fc1cf1f7
Ron Gross <chiefninjaofficer@gmail.com>
2017-07-18 16:52:45 +0300

    Move API to separate file

---
+++ api.js
@@ -0,0 +1,16 @@
+var express = require('express');        // call express
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function (req, res) {
+    res.json({ message: 'Welcome to Satoshi api server!' });
+});
+router.get('/login', function (req, res) {
+    res.json({ message: 'You probably want to use HTTP POST' });
+});
+
+// more routes for our API will happen here
+module.exports = router;
--- server.js
+++ server.js
@@ -15,23 +15,11 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function (req, res) {
-    res.json({ message: 'Welcome to Satoshi api server!' });
-});
-router.get('/login', function (req, res) {
-    res.json({ message: 'You probably want to use HTTP POST' });
-});
-
-// more routes for our API will happen here
+var api = require('./api.js');
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/api', api);
 
 // START THE SERVER
 // =============================================================================
