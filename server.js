e3506d8afbd543257ffdfa11caeda86332acff31
Ron Gross <ron.gross@gmail.com>
2017-07-19 19:12:46 +0300

    Log POST body params

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var bodyParser = require('body-parser');
 
 // Logger
 var logger = function (req, res, next) {
-    console.log(req.method + JSON.stringify(req.query) + JSON.stringify(req.params));
+    console.log(req.method + JSON.stringify(req.query) + JSON.stringify(req.body) + JSON.stringify(req.params));
     next(); // Passing the request to the next handler in the stack.
 }
 app.use(logger); // Here you add your logger to the stack.
