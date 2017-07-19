2f1a40712cdc39b8735227cc4208bee48e30548a
Ron Gross <ron.gross@gmail.com>
2017-07-19 18:28:58 +0300

    Log request parameters

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ var express = require('express');        // call express
 var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
+// Logger
+var logger = function (req, res, next) {
+    console.log(req.method + JSON.stringify(req.query) + JSON.stringify(req.params));
+    next(); // Passing the request to the next handler in the stack.
+}
+app.use(logger); // Here you add your logger to the stack.
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
